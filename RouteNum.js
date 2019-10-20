fc31a7943adba2bd31800f64c99367a87a1658b8
khogan24 <55300711+khogan24@users.noreply.github.com>
2019-10-19 19:30:10 -0500

    Add files via upload

---
+++ RouteNum.js
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//<! key = Bp3C5aECiQ5gqEkyC83LNhqV6>
+function RouteNum() {
+	
+	constructor(JsonGetter)
+	{
+		super(JsonGetter);
+		this.state=
+		{
+			items: [],
+		
+		}
+	}
+
+  return (
+   
+  );
+}
+
+export default App;
